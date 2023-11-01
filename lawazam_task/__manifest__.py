fbc78266b28ea7679ee689edab1e4bffb7e7558a
Asmaa Aly <asmaa.cs20106@gmail.com>
2023-11-01 07:32:03 +0200

    Done Task

---
+++ lawazam_task/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Capturing Bank fee in Payments',
+    'version': '1.1',
+    'author': 'Asmaa Aly',
+    'description': """
+    LAWAZAM Task Module
+""", 
+    'depends': [
+        'payment', 'account'
+
+    ],
+    'data': [
+        # views
+        'views/payment.xml',
+
+    ]
+
+}
